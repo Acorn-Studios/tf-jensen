Fixes for exploding gradients from relu to tanh. Also using mse loss.

train-nightwatch.py
@@ -35,6 +35,9 @@ df = df.drop(['tick', 'name', 'steam_id'], axis=1)  # Keep only numerical values
 # Handle NaN values in `va_delta` and `pa_delta` (fill with 0 or column mean)
 df['va_delta'] = df['va_delta'].fillna(0)  # Replace NaN with 0
 df['pa_delta'] = df['pa_delta'].fillna(0)  # Replace NaN with 0
+# Find nans and print them
+nans = df.isna().sum()
+print(nans[nans > 0])
 
 # Ensure the DataFrame is not empty
 if df.empty:
@@ -65,24 +68,28 @@ size = 8 # Size of the model
 def build_lstm_autoencoder(input_shape, size=1):
     model = Sequential()
     model.add(Input(shape=(input_shape[1], input_shape[2])))
-    model.add(LSTM(128*size, activation='relu', return_sequences=True))
-    model.add(LSTM(64*size, activation='relu', return_sequences=False))
+    model.add(LSTM(128*size, return_sequences=True))
+    model.add(LSTM(64*size, return_sequences=False))
     model.add(RepeatVector(input_shape[1]))
-    model.add(LSTM(64*size, activation='relu', return_sequences=True))
-    model.add(LSTM(128*size, activation='relu', return_sequences=True))
+    model.add(LSTM(64*size, return_sequences=True))
+    model.add(LSTM(128*size, return_sequences=True))
     model.add(TimeDistributed(Dense(input_shape[2], activation='tanh')))
     return model
 
 input_shape = X_train.shape
 lstm_autoencoder = build_lstm_autoencoder(input_shape, size=size)
 lstm_autoencoder.summary()
-lstm_autoencoder.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-
-# Implement ReduceLROnPlateau callback
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, clipnorm=1.0)
+lstm_autoencoder.compile(optimizer=optimizer, loss='mse')
 
 # Train the Autoencoder
-history = lstm_autoencoder.fit(X_train, X_train, epochs=3, batch_size=32, validation_data=(X_test, X_test), callbacks=[reduce_lr])
+history = lstm_autoencoder.fit(
+	X_train, X_train, 
+	epochs=3, 
+	batch_size=32, 
+	validation_data=(X_test, X_test), 
+	callbacks=[ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)]
+)
 
 # Save the model
 lstm_autoencoder.save(f'jensen-nightwatch-v2-s{size}-lstm.keras')